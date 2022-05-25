8546437ace9389f0a63dc5a355f8eea6d6716a64
Kai Fang <redbulldown@gmail.com>
2022-05-25 15:37:21 +0800

    Update deployment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import WeatherLookup from "./components/WeatherLookup";
 
 function App() {
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=35&appid=bac8236c7a5c5766de9c8c5415751152"
-
   return (
     <div className="app">
       <WeatherLookup />
